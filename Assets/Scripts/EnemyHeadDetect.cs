3f6459d44496389d4d9538c0e7a3518bd7126a7b
dpalomoe <91782530+dpalomoe@users.noreply.github.com>
2021-11-08 23:53:56 +0100

    add small push after jump over an enemy

--- Assets/Scripts/EnemyHeadDetect.cs
+++ Assets/Scripts/EnemyHeadDetect.cs
@@ -6,19 +6,23 @@ public class EnemyHeadDetect : MonoBehaviour
 {
 
     GameObject Enemy;
-    private Animator anim;
     private Frog frog;
 
     // Start is called before the first frame update
     void Start()
     {
         Enemy = gameObject.transform.parent.gameObject;
-        anim = Enemy.GetComponent<Animator>();
         frog = Enemy.GetComponent<Frog>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        var magnitude = 350;
+
+        var force = transform.position - collision.transform.position;
+        force.Normalize();
+        collision.transform.GetComponent<Rigidbody2D>().AddForce(-force * magnitude);
+        GetComponent<BoxCollider2D>().enabled = false;
         frog.Die();
     }
 }
